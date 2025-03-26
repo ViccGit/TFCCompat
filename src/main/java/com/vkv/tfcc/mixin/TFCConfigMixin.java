ceb95b94ed1f18c9398c2327f2611864f9daf804
Vicc <davidandrewpunzalan@gmail.com>
2025-03-26 18:23:25 -0400

    Upload

--- src/main/java/com/vkv/tfcc/mixin/TFCConfigMixin.java
+++ src/main/java/com/vkv/tfcc/mixin/TFCConfigMixin.java
@@ -8,8 +8,32 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(ServerConfig.class)
 public class TFCConfigMixin {
     @ModifyArg(method = "<init>(Lnet/dries007/tfc/config/ConfigBuilder;)V", at = @At(value = "INVOKE",
-            target = "Lnet/dries007/tfc/config/ConfigBuilder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;"), index = 1)
-    private boolean yes(boolean value) {
+            target = "Lnet/dries007/tfc/config/ConfigBuilder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;", ordinal = 0), index = 1)
+    private boolean portal(boolean value) {
         return true;
+    }
+
+    @ModifyArg(method = "<init>(Lnet/dries007/tfc/config/ConfigBuilder;)V", at = @At(value = "INVOKE",
+            target = "Lnet/dries007/tfc/config/ConfigBuilder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;", ordinal = 43), index = 1)
+    private boolean chiselCollapse(boolean value) {
+        return false;
+    }
+
+    @ModifyArg(method = "<init>(Lnet/dries007/tfc/config/ConfigBuilder;)V", at = @At(value = "INVOKE",
+            target = "Lnet/dries007/tfc/config/ConfigBuilder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;", ordinal = 42), index = 1)
+    private boolean landslide(boolean value) {
+        return false;
+    }
+
+    @ModifyArg(method = "<init>(Lnet/dries007/tfc/config/ConfigBuilder;)V", at = @At(value = "INVOKE",
+            target = "Lnet/dries007/tfc/config/ConfigBuilder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;", ordinal = 40), index = 1)
+    private boolean collapse(boolean value) {
+        return false;
+    }
+
+    @ModifyArg(method = "<init>(Lnet/dries007/tfc/config/ConfigBuilder;)V", at = @At(value = "INVOKE",
+            target = "Lnet/dries007/tfc/config/ConfigBuilder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;", ordinal = 41), index = 1)
+    private boolean collapseExplode(boolean value) {
+        return false;
     }
 }
